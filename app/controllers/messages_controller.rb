8d05d330f848c5e5e34d95b247e75c64b1843270
Jason Dancocks <jason.dancocks@gmail.com>
2018-04-05 19:02:20 +0100

    add styles for splash screen on homepage

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,22 @@
 class MessagesController < ApplicationController
-   def new
+  def new
     @message = Message.new
-   end
-    def create
-        @message = Message.new(message_params)
-        
-        if @message.valid?
-            MessageMailer.contact(@message).deliver_now
-            redirect_to root_path
-        else
-            render :new
-        end
-            
-    end
+  end
 
-    private
-    
+  def create
+    @message = Message.new(message_params)
 
-    def message_params
-        params.require(:message).permit(:name,:email,:body)
+    if @message.valid?
+      MessageMailer.contact(@message).deliver_now
+      redirect_to root_path
+    else
+      render :new
     end
-    
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :body)
+  end
 end
